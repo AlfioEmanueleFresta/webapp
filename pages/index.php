67a661bcaf0346cf02d581bce6f9a5b9f23042c5
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-07-06 15:42:46 +0100

    Recent articles on home page

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,32 @@
 
-<h2>This is the home page.</h2>
-<p>Please find a list of the latest articles from our blog.</p>
+<h2>Latest articles</h2>
+<p>Here is a list of the most recent articles from our website!</p>
+<p>&nbsp;</p>
+
+<?php
+$query = $db->query("
+    SELECT    articles.id as article_id, 
+              timestamp, author_id, 
+              username as author_username,
+              title, body
+    FROM      articles, users
+    WHERE     articles.author_id = users.id
+    ORDER BY  timestamp DESC
+    LIMIT     0, 10
+");
+$query->execute();
+
+$articles = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ( $articles as $article ) { ?>
+
+    <h4><?= $article["title"]; ?></h4>
+    <p>Posted by <?= $article["author_username"]; ?> on <?= date('d/m/Y H:i', $article["timestamp"]); ?></p>
+    <p>
+        <a href="?page=read.php&article_id=<?= $article["article_id"]; ?>">
+            Read the article &raquo;
+        </a>
+    </p>
+    <p>&nbsp;</p>
+
+<?php } ?>

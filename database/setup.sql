d5b8dd0783940cec6eb5cfbcf8432a68da959f5d
James <jrwb502@york.ac.uk>
2016-07-11 17:36:38 +0100

    Added new entries to database

--- database/setup.sql
+++ database/setup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 11, 2016 at 03:32 PM
+-- Generation Time: Jul 11, 2016 at 04:34 PM
 -- Server version: 5.5.49-0+deb8u1
 -- PHP Version: 5.6.22-0+deb8u1
 
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `role` varchar(32) NOT NULL DEFAULT 'user',
   `hint` varchar(255) NOT NULL,
   `salt` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -110,7 +110,27 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`, `hint`, `salt`) VALUE
 (32, 'a017', '6138713d6348742be9057d72abccf62c0aea85f301bc98e241dbd7d2fb60a084', 'user', 'River', 'This is the salt for exercise 2'),
 (33, 'a018', '8d04afe1e6e71b17defca7ba4932042869cb144a440775aa974c9ac18d90f8f8', 'user', 'Who?', 'This is the salt for exercise 2'),
 (34, 'a019', 'f930987f760bdec540657c43c0205c060b8e1ec5c76dcdc3de30946bcccae99d', 'user', '', 'This is the salt for exercise 2'),
-(35, 'a020', '70e9299e9167af67a01b0e9229b3226b49069a84f3609b966c5b362d8ea2bbfc', 'user', 'Favourite band', 'This is the salt for exercise 2');
+(35, 'a020', '70e9299e9167af67a01b0e9229b3226b49069a84f3609b966c5b362d8ea2bbfc', 'user', 'Favourite band', 'This is the salt for exercise 2'),
+(100, 'b001', '7f5214fe89bfb7626bdafad41eeed501d58f7eb25338b22b24a7aca5f3bbaa79', 'staff', '', 'bc95aba9fbb626e2e2edd18f5b07444ea4ee96ee1a1f78e0ab1800bc6ff97eaa'),
+(101, 'b002', 'ad524378892eec75e96f4813148cf58a6922a485af8b28797613cea01248f489', 'user', '', 'e926b29e73099c9345692b71cba70b9120afe7ba2900ff119fc03f5b3efc29d3'),
+(102, 'b003', '49029a52333fddd6d7410eb34d90e3f0bcc8c57e71ee61eccdb9149438f3eeeb', 'user', '', '951b45c744166533222f9354393397fa32cba848c5cbcd38b7be78910a14c230'),
+(103, 'b004', 'f4743ef86bc50216852edc447c7186f425db0d83f301d8ff25ae89b3f4e7e642', 'user', '', '71dfed2e0ae0409f5c4cefc2a231b2a21b44b57a544863e58ad1cda6e6017d32'),
+(104, 'b005', '0cd026f659531e3dc573cb6fed017781f54c3e0942b9870945911fe611040d9f', 'user', '', 'b316757141b275c3f9312e7d74ce66a41400b010537f3558d7beec5b5322a0a2'),
+(105, 'b006', '1fc6e1b06e97f3f566a6ef1bc8032e2abc473b8aff04b63418fc0f9b87aebe87', 'user', '', 'f679e513ef3f979446cd7cbebff846c8dfa4a685459889f913915ef40dd8e926'),
+(106, 'b007', '5be2e27c0095b488a64df993bab69929a1a7cff262c76fc58b878fe5a6413c76', 'user', '', '4fff251de28027843919b10944b1129289e951462f9dc0b8d267a18bb29a8fb1'),
+(107, 'b008', 'c512172bbd4c2d16cbcdde6f923cc8317ff7dd50359c0cae863346f584578e2d', 'user', '', '9ddd450b1e85983ac70e40a35056a129423fa0d96d77fc915de55d34d928b34a'),
+(108, 'b009', 'e812ce69ea07900a7ec1dd1d9cc1c0d69f609340bd0481d1cf81a301866ea89f', 'user', '', '34f17c194bca94472ffd3933d80d300b7602965de1fb90788b8cb0dd30ed7ec3'),
+(109, 'b010', '4fc63e4a0b4286ee6ef6f87e0919c921516348dbc96e5cd0995fa02f2e2518aa', 'user', '', '40e7fcebd8f328241e1d45d72884d709e3496bdc50d50f2130491b311a3a0679'),
+(110, 'b011', '6b788cbf82b79f9e3cbf92ce99ce21a5f07921705b9b041542bb300d9ef13e2b', 'user', '', '7d787f88c24b070e25760caf654afcc991095501edacab65a873cf175206bc45'),
+(111, 'b012', '775776031e160e402924a4973a8eb1c66124c758f60b089e797a2feda834ad41', 'user', '', 'a83c3e6dee5f21dffe7530b7ee63201ca67a0466f1f5a15729d30527a9eb06c6'),
+(112, 'b013', '1712ee91936b9e46ae8e13ba84220bf207db6bb250822b1b047392cbadbc50d2', 'user', '', 'b4e9eead7aa1caea3c9b46c5ff34f063b73b44e2e5c3370748d179cc594a9dd8'),
+(113, 'b014', '8c57522f9c48be4f0d8ee5e37be9e3b7ac43a0c57e357de24c9d6222e81ecf98', 'user', '', '31407f5e0de6fee957b33e254b5d6c317d4bf3e2de2ef301f1210210c2c52e83'),
+(114, 'b015', '446473cce1c6698e3618dd8f945057d0f8155c7060b685da5c5d722e814fe933', 'user', '', 'f9230b12a6d9e5c1bee695cfb2336f624ef2809e58c9342e1262c2ffcc2db40a'),
+(115, 'b016', '4980e16e49692e8bd30b4068514da69e425fb233d4f565bd42bf8b021e896aba', 'user', '', '0f399071ba74ddb0e3d4a312801339896032762b3dccacb026c5a1f92852e639'),
+(116, 'b017', '2093f51839004e6e4225dfaf446fc05593dd234e30ca2a5369e9d7843a81d2cc', 'user', '', '2c7bd3f31dc6e861cf11d2f282f5dc962c964b704241215826798f6a8abbe68f'),
+(117, 'b018', '7fd4c53efdb50f7add0cd6cfc7e4e52c1b0b92b8ef8f4d5dcc106414220e8ddb', 'user', '', '7493180e1d29353f5fe2c0a6af5481986fd7341efdd58db37c35cf7b812ab85a'),
+(118, 'b019', 'f33a2af31b03835a2bc5a0affa2d05a9567b284bed7f748a47fc46f62be1686a', 'user', '', '02e0bd919da4513a740fe45a56585fd0f05886c3e6c93ce111be2d34109f22ae'),
+(119, 'b020', 'a79618cca95fac35b431291f1c1714fd515347c35998ebded42feefd48266cc5', 'user', '', '5081e26fe85b2d637e524c37cc67ebadeb6a013fc718b479b41dbaa38547f9d8');
 
 --
 -- Indexes for dumped tables
@@ -130,7 +150,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=120;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
